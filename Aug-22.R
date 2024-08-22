c3478f5b8ec51cc5a02469638d18bcf9f8bce03d
joelmore19 <111890592+joelmore19@users.noreply.github.com>
2024-08-22 16:27:25 -0400

    Add files via upload

---
+++ Aug-22.R
@@ -0,0 +1,13 @@
+#Author: Joshua Elmore, Date: 08/22/24, Purpose: Test the measures of descriptive stats
+
+#How to calculate standard deviation in R
+
+X <- c(1,2,3,4,5)
+sd(X)
+
+#How to calculate mean in R
+
+mean(X)
+
+#How to calculate median in R
+median(X)
